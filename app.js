aa822a01deec1d9c7c2ffc0f0f76f47f1154e2bc
Angel Victoria <angel.victoria92+git@gmail.com>
2021-01-24 17:30:39 -0600

    Transistioned track calls to RESTful routes

--- app.js
+++ app.js
@@ -30,11 +30,20 @@ const timingSchema = new mongoose.Schema ({
 
 const Timing = mongoose.model("Timing", timingSchema);
 
-app.get("/", (req, res) => {
-  console.log(req.query);
-  const query = req.query
+app.get("/tracks", (req,res) => {
+  Timing.find().limit(100).exec((err, timings) => {
+    if (err) return handleError(err);
+    res.render('timingBoard', {
+      timings: timings
+    });
+  });
+})
+
+app.get("/tracks/:trackID", (req, res) => {
+  const trackID = req.params.trackID;
+  console.log(trackID);
   Timing.find({
-    trackName: query.track.charAt(0).toUpperCase() + query.track.slice(1).toLowerCase()
+    trackName: trackID.charAt(0).toUpperCase() + trackID.slice(1).toLowerCase()
   }).limit(100).exec((err,timings) => {
     if (err) return handleError(err);
     res.render('timingBoard', {
@@ -42,6 +51,23 @@ app.get("/", (req, res) => {
     });
   });
 });
+
+app.post("/tracks", (req, res) => {
+  const entry = new Timing({
+    driverName: req.body.driverName,
+    trackName: req.body.trackName,
+    trackConfig: req.body.trackConfig,
+    car: req.body.car,
+    lapTime: req.body.lapTime
+  });
+  entry.save((err)=>{
+    if(!err){
+      res.send("Successfully added a new article");
+    } else {
+      res.send(err);
+    }
+  });
+})
 
 app.listen(LOCAL_PORT, () => {
   console.log("Server running on port " + LOCAL_PORT)
